8a2a5b77e3cfd7cd4532441410315c0d0cddc298
nicola-decao <nicola.decao@studenti.unipd.it>
2016-12-02 19:33:18 +0100

    multiple nets import

--- src/main/java/fuoco/FuocoCore.java
+++ src/main/java/fuoco/FuocoCore.java
@@ -12,17 +12,34 @@ import java.io.IOException;
 
 public class FuocoCore implements Core {
 
-    private MultiLayerNetwork net;
+    private MultiLayerNetwork[] nets;
 
-    private static double sigmoid(double x) {
-        return (1/( 1 + Math.pow(Math.E,(-1*x))));
+//    private static double sigmoid(double x) {
+//        return (1/( 1 + Math.pow(Math.E,(-1*x))));
+//    }
+
+    private double[] computeAction(INDArray sensors) {
+        double[] predictions = new double[]{0, 0};
+        double[] steering = new double[nets.length];
+        double[] accelBrake = new double[nets.length];
+
+        for (int i = 0; i < nets.length; i++) {
+            INDArray prediction = nets[i].output(sensors);
+            steering[i] = prediction.getDouble(0);
+            accelBrake[i] = prediction.getDouble(1);
+        }
+
+        predictions[0] = Nd4j.create(steering).mean().getDouble(0);
+        predictions[1] = Nd4j.create(accelBrake).min().getDouble(0);
+
+        return predictions;
     }
 
     public Action computeAction(SensorModel sensors) {
         Action action = new Action();
         double[] d = new double[29];
 
-        d[0] = sensors.getAngleToTrackAxis()/3.1416;
+        d[0] = sensors.getAngleToTrackAxis()/Math.PI;
         for (int i = 1; i < 20; i++) {
             d[i] = sensors.getTrackEdgeSensors()[i - 1]/200.0;
         }
@@ -35,17 +52,23 @@ public class FuocoCore implements Core {
         }
         d[28] = sensors.getRPM()/10000.0;
 
-        INDArray predicted = net.output(Nd4j.create(d));
+        double[] predicted = computeAction(Nd4j.create(d));
 
-        action.steering = Math.tanh(predicted.getDouble(0));
-        action.accelerate = sigmoid(predicted.getDouble(1));
-        action.brake = sigmoid(predicted.getDouble(2));
+        action.steering = predicted[0];
+
+        if (predicted[1] >= 0) {
+            action.accelerate = predicted[1];
+            action.brake = 0;
+        } else {
+            action.accelerate = 0;
+            action.brake = -predicted[1];
+        }
 
         return action;
     }
 
     public void loadGenome(IGenome genome) {
-        net = ((FuocoCoreGenome) genome).getNet();
+        nets = ((FuocoCoreGenome) genome).getNets();
     }
 
     public IGenome getGenome() throws IOException {
--- src/main/java/fuoco/FuocoCoreGenome.java
+++ src/main/java/fuoco/FuocoCoreGenome.java
@@ -14,6 +14,7 @@ import org.nd4j.linalg.factory.Nd4j;
 import org.nd4j.linalg.lossfunctions.LossFunctions;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
@@ -21,9 +22,9 @@ import java.util.List;
 
 public class FuocoCoreGenome implements IGenome {
     private static final long serialVersionUID = 6534186543165341653L;
-    private MultiLayerNetwork net;
+    private MultiLayerNetwork[] nets;
 
-    private static INDArray read(String file) throws IOException {
+    private static INDArray read(File file) throws IOException {
         List<Double> l = new ArrayList<>();
         BufferedReader br = new BufferedReader(new FileReader(file));
         String line;
@@ -31,37 +32,47 @@ public class FuocoCoreGenome implements IGenome {
             l.add(Double.parseDouble(line));
         }
         double[] r = new double[l.size()];
-        for(int i = 0; i < l.size(); i++) {
+        for (int i = 0; i < l.size(); i++) {
             r[i] = l.get(i);
         }
         return Nd4j.create(r);
     }
 
-    FuocoCoreGenome(String file) throws Exception {
-        this.net = new MultiLayerNetwork(new NeuralNetConfiguration.Builder()
-                .list()
-                .layer(0, new DenseLayer.Builder()
-                        .nIn(29)
-                        .nOut(300)
-                        .activation("relu")
-                        .build())
-                .layer(1, new DenseLayer.Builder()
-                        .nIn(300)
-                        .nOut(600)
-                        .activation("relu")
-                        .build())
-                .layer(2, new OutputLayer.Builder()
-                        .activation("identity")
-                        .nIn(600)
-                        .nOut(3)
-                        .build())
-                .pretrain(false)
-                .build());
-        this.net.init(read(file), true);
+    FuocoCoreGenome(String path) throws Exception {
+
+        File dir = new File(path);
+        File[] files = dir.listFiles();
+
+        assert files != null;
+
+        nets = new MultiLayerNetwork[files.length];
+
+        for (int i = 0; i < files.length; i++) {
+            nets[i] = new MultiLayerNetwork(new NeuralNetConfiguration.Builder()
+                    .list()
+                    .layer(0, new DenseLayer.Builder()
+                            .nIn(29)
+                            .nOut(200)
+                            .activation("relu")
+                            .build())
+                    .layer(1, new DenseLayer.Builder()
+                            .nIn(200)
+                            .nOut(200)
+                            .activation("relu")
+                            .build())
+                    .layer(2, new OutputLayer.Builder()
+                            .activation("tanh")
+                            .nIn(200)
+                            .nOut(2)
+                            .build())
+                    .pretrain(false)
+                    .build());
+            nets[i].init(read(files[i]), true);
+        }
     }
 
-    public MultiLayerNetwork getNet() {
-        return net;
+    public MultiLayerNetwork[] getNets() {
+        return nets;
     }
 }
 
