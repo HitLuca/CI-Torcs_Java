150d250e0ffef59f83c613fc45d366cf264259c4
HitLuca <luca.simonetto.94@gmail.com>
2016-12-09 18:46:36 +0100

    baseline for pair driving

---
+++ src/main/java/fuoco/test.java
@@ -0,0 +1,12 @@
+package fuoco;
+
+import java.io.IOException;
+
+/**
+ * Created by luca on 12/9/16.
+ */
+public class test {
+    public static void main(String[] args) throws IOException, InterruptedException {
+        PairDriving driver = new PairDriving();
+    }
+}
